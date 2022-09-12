86fddec623dcf5622a0a5d25c9a90de663f66888
Raphael <raphael.tecnologiainformacaorj@gmail.com>
2022-09-12 02:26:36 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,11 +20,17 @@ function SalesCard() {//criando a tabela
 
 
     useEffect(() => {//("http://localhost:8080/sales")
-        axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        console.log(dmin);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
